e70e52a5dc02dc6a38b99e10fa2b06f6a7b8f12c
Erwin <54679426+Eroldin@users.noreply.github.com>
2024-03-06 19:39:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ yay -S bc rsync mlocate bash-completion pkgstats zip unzip unrar p7zip lzop cpio
 
 yay -S --asdeps --needed cairo fontconfig freetype2
 
+# change vbox drivers to your video card
 yay -S dmidecode virtualbox-guest-utils mesa-libgl xf86-video-vmware cups cups-pdf gutenprint ghostscript gsfonts foomatic-db foomatic-db-engine foomatic-db-nonfree foomatic-db-ppds foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds sddm sddm-kcm cinnamon nemo-fileroller nemo-preview gnome-screenshot gedit gnome-terminal gnome-control-center gnome-system-monitor gnome-power-manager mintlocale pipewire pipewire-alsa pipewire-pulse lib32-pipewire lib32-alsa-lib lib32-alsa-plugins archlinux-artwork archlinux-wallpaper keepassxc xviewer flatpak pamac-nosnap gnome-calculator-gtk3 yt-dlp candy-icons-git sweet-kde-git sweet-gtk-theme sweet-gtk-theme-dark masterpdfeditor-free dnsmasq networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc network-manager-applet nm-connection-editor gnome-keyring bluez bluez-utils gparted ufw gufw icoutils wine wine_gecko wine-mono winetricks gimp simple-scan google-chrome firefox transmission-gtk thunderbird easytag mpv vlc handbrake-gtk gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav libdvdnav libdvdcss cdrdao cdrtools ffmpeg ffmpegthumbnailer ffmpegthumbs ttf-ms-fonts noto-fonts-cjk --needed
 
 yay -Rsc --noconfirm $(yay -Qdqt)
@@ -18,6 +19,7 @@ sudo systemctl gufw
 sudo systemctl enable sddm
 sudo systemctl enable bluetooth.service
 sudo systemctl set-default graphical.target
+# if not needed, remove vbox modules
 sudo modprobe vboxguest vboxsf vboxvideo virtualbox-guest uinput
 sudo usermod "$USER" -aG vboxsf
 sudo timedatectl set-ntp true

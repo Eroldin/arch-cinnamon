d028cf61596aa4b1f3054254d45c264db2c5e56b
Erwin <54679426+Eroldin@users.noreply.github.com>
2024-03-14 19:52:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ yay -S --asdeps --needed --noconfirm cairo fontconfig freetype2
 
 yay -S dmidecode mesa-libgl cups cups-pdf system-config-printer gutenprint watchdog breeze gnome-themes-extra xdg-user-dirs-gtk cinnamon-sounds ghostscript gsfonts foomatic-db foomatic-db-engine foomatic-db-nonfree foomatic-db-ppds foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds sddm sddm-kcm cinnamon nemo-fileroller nemo-preview gnome-screenshot gedit gnome-terminal-transparency gnome-control-center gnome-system-monitor gnome-power-manager mintlocale pipewire pipewire-alsa pipewire-pulse lib32-pipewire lib32-alsa-lib lib32-alsa-plugins archlinux-artwork archlinux-wallpaper keepassxc xviewer flatpak pamac-nosnap gnome-calculator yt-dlp candy-icons-git sweet-kde-git sweet-gtk-theme sweet-gtk-theme-dark masterpdfeditor-free dnsmasq networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc network-manager-applet nm-connection-editor gnome-keyring bluez bluez-utils gparted ufw gufw icoutils wine wine_gecko wine-mono winetricks gimp simple-scan google-chrome firefox transmission-gtk thunderbird easytag mpv vlc handbrake-gtk gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav libdvdnav libdvdcss cdrdao cdrtools ffmpeg ffmpegthumbnailer ffmpegthumbs ttf-ms-fonts noto-fonts-cjk --needed --noconfirm
 
+# If using Gnome Boxes
+# yay -S --needed --noconfirm spice spice-gtk spice-protocol spice-vdagent
 yay -Rsc --noconfirm $(yay -Qdqt)
 
 sudo sddm --example-config | sudo tee /etc/sddm.conf

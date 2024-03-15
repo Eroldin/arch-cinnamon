6ee9c2dca8bea86bce7a74fcc88458c1cf2a565f
Erwin <54679426+Eroldin@users.noreply.github.com>
2024-03-15 14:41:12 +0100

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/zsh
 
 set -e
 
 pacstrap /mnt man zsh zsh-completions grml-zsh-config ntfs-3g gdisk util-linux grub efibootmgr amd-ucode base base-devel linux linux-headers linux-lts linux-lts-headers networkmanager wget nano git curl zram-generator reflector smbclient linux-firmware linux-firmware-whence btrfs-progs xfsprogs lvm2
 pacstrap /mnt kernel-modules-hook
-arch-chroot /mnt "pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com;pacman-key --lsign-key 3056513887B78AEB;pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm;pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm"
+arch-chroot /mnt zsh -c "pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com;pacman-key --lsign-key 3056513887B78AEB;pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm;pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm"
 
 cat <<-EOF > /mnt/etc/locale.conf
 	LANG=en_US.UTF-8
@@ -20,7 +20,7 @@ cat <<-EOF > /mnt/etc/locale.conf
 EOF
 
 cat /etc/pacman.conf >> /mnt/etc/pacman.conf
-cat <<-EOF > /etc/systemd/zram-generator.conf
+cat <<-EOF > /mnt/etc/systemd/zram-generator.conf
 	[zram0]
 	zram-size = ram / 2
 	compression-algorithm = zstd

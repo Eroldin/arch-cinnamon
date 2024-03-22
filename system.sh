80162155cf6b22723ce0bbacea34066a27c6935a
Erwin <54679426+Eroldin@users.noreply.github.com>
2024-03-22 19:26:18 +0100

    Update system.sh

--- system.sh
+++ system.sh
@@ -27,6 +27,7 @@ cat <<-'EOF' > /etc/pacman.d/mirrorlist
 EOF
 
 # Installing the base system and the Yay AUR Wrapper. The Chaotic-Aur repo is included.
+pacman -Syy
 pacstrap /mnt man zsh zsh-completions grml-zsh-config ntfs-3g gdisk util-linux grub efibootmgr amd-ucode base base-devel linux-zen linux-zen-headers linux-lts linux-lts-headers networkmanager wget nano git curl zram-generator reflector smbclient linux-firmware linux-firmware-whence btrfs-progs xfsprogs lvm2
 pacstrap /mnt kernel-modules-hook
 arch-chroot /mnt zsh -c "pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com;pacman-key --lsign-key 3056513887B78AEB && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm"

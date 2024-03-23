c1ef6c36a203397905055cab0a482ff0dae3cd57
Erwin <54679426+Eroldin@users.noreply.github.com>
2024-03-23 19:15:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,12 @@ yay -S --asdeps --needed --noconfirm cairo fontconfig freetype2
 yay -S bc rsync mlocate bash-completion pkgstats zip unzip unrar p7zip lzop cpio avahi nss-mdns alsa-utils alsa-plugins dosfstools exfat-utils f2fs-tools fuse fuse-exfat mtpfs xorg-server xorg-apps xorg-xinit xorg-xkill xorg-xinput xf86-input-libinput mesa weston xorg-server-xwayland --needed --noconfirm
 yay -S --needed --noconfirm  dmidecode mesa-libgl cups cups-pdf system-config-printer gutenprint nano-syntax-highlighting watchdog breeze xdg-desktop-portal-gtk noto-fonts-emoji gnome-themes-extra ghostlexly-gpu-video-wallpaper xwinwrap-0.9-bin xdg-user-dirs-gtk cinnamon-sounds ghostscript gsfonts foomatic-db foomatic-db-engine foomatic-db-nonfree foomatic-db-ppds foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds sddm sddm-kcm cinnamon nemo-fileroller nemo-preview gnome-screenshot gedit gnome-terminal-transparency gnome-control-center gnome-system-monitor gnome-power-manager mintlocale pipewire pipewire-alsa pipewire-pulse lib32-pipewire lib32-alsa-lib lib32-alsa-plugins archlinux-artwork archlinux-wallpaper keepassxc xviewer flatpak pamac-nosnap gnome-calculator yt-dlp candy-icons-git sweet-kde-git sweet-gtk-theme sweet-gtk-theme-dark masterpdfeditor-free dnsmasq networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc network-manager-applet nm-connection-editor gnome-keyring bluez bluez-utils gparted ufw gufw icoutils wine wine_gecko wine-mono winetricks gimp simple-scan google-chrome firefox transmission-gtk thunderbird easytag mpv vlc handbrake-gtk gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav libdvdnav libdvdcss cdrdao cdrtools ffmpeg ffmpegthumbnailer ffmpegthumbs ttf-ms-fonts noto-fonts-cjk #spice spice-gtk spice-protocol spice-vdagent
 sudo ln -sf /usr/lib/libalpm.so.14.0.0 /usr/lib/libalpm.so.13
+yay -S --noconfirm --needed btrfs-assistant
+yay -S --noconfirm --needed grub-btrfs
+yay -S --noconfirm --needed snap-pac-git
+yay -S --noconfirm --needed snapper
+yay -S --noconfirm --needed snapper-tools-git
+yay -S --noconfirm --needed snapper-support
 yay -Rsc --noconfirm $(yay -Qdqt)
 
 sudo sddm --example-config | sudo tee /etc/sddm.conf
@@ -22,6 +28,7 @@ sudo systemctl set-default graphical.target
 sudo timedatectl set-ntp true
 sudo systemctl enable avahi-daemon.service
 
+sudo snapper -c root create-config /
 sudo wget -q -O /usr/share/backgrounds/mylivewallpapers.com-Night-Elf-Warcraft-3-Reforged.mp4 "https://mylivewallpapers.com/?ddownload=18246"
 sudo chmod 644 /usr/share/backgrounds/mylivewallpapers.com-Night-Elf-Warcraft-3-Reforged.mp4
 sudo bash -c 'cat >> /etc/nanorc' <<-EOF
